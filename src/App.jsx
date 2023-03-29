376489909097b7c0a02e67b74caeee0e605d2334
Gladis <gladis@Equipo1.com>
2023-03-29 13:10:00 -0600

    Iniciando

---
+++ src/App.jsx
@@ -0,0 +1,46 @@
+function App(){
+   return (
+    <div className="container text-center">
+      <div className="row">
+        <div className="col">
+      <h1>Shoppin List</h1>
+      <hr />
+      </div>
+      </div>
+      <div className="row">
+        <div className="col-3">
+        C.
+      </div>
+      </div>
+
+      <div className="row"> 
+        <div className="col-8">
+          Primer elemento de mi lista
+      </div>
+      <div className="col-3">
+          Botones
+      </div>
+      </div>
+
+      <div className="row">
+        <div className="col-3">
+        C.
+      </div>
+      </div>
+      <div className="row">
+        <div className="col-8">
+          Segundo elemento de mi lista
+      </div>
+      <div className="col-3">
+          Botones
+      </div>
+      </div>
+
+
+
+
+    </div> 
+   )
+}
+
+export default App
