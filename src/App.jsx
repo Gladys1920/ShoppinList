5ff9cb1ede6c6a8d26729e8c595d096ff9ec6c15
Gladis <gladis@Equipo1.com>
2023-03-30 14:03:44 -0600

    Alineacion de texto

--- src/App.jsx
+++ src/App.jsx
@@ -8,38 +8,47 @@ function App(){
       </div>
       </div>
       <div className="row">
-        <div className="col-3">
-        C.
+        <div className="col-1">
+          <input type="checkbox" />
       </div>
+      <div className="col text-start">
+      1 kg
       </div>
+        <div className="col-5 col-md-7 text-start ">
+          Tortillas
+      </div>
+      <div className="col-4 col-md-3 btn-group btn-group-sm">
+        <button className="btn btn-outline-primary">
+        < i className="bi bi-pencil-square"></i>
+        </button>
+        <button className="btn btn-outline-danger">
+          <i className="bi bi-trash2-fill"></i>
+        </button>
+      </div>
+    </div>
 
-      <div className="row"> 
-        <div className="col-8">
-          Primer elemento de mi lista
+    <div className="row">
+        <div className="col-1">
+          <input type="checkbox" />
       </div>
-      <div className="col-3">
-          Botones
+      <div className="col text-start">
+      1 lt
       </div>
+        <div className="col-5 col-md-7 text-start">
+          Aceite
       </div>
-
-      <div className="row">
-        <div className="col-3">
-        C.
+      <div className="col-4 col-md-3 btn-group btn-group-sm text-end">
+        <button className="btn btn-outline-primary">
+        < i className="bi bi-pencil-square"></i>
+        </button>
+        <button className="btn btn-outline-danger">
+          <i className="bi bi-trash2-fill"></i>
+        </button>
       </div>
-      </div>
-      <div className="row">
-        <div className="col-8">
-          Segundo elemento de mi lista
-      </div>
-      <div className="col-3">
-          Botones
-      </div>
-      </div>
+    </div>
 
 
-
-
-    </div> 
+  </div> 
    )
 }
 
